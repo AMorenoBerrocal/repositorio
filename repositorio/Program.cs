b845e732d09222a1e018ff9dfcb1f6efb4cec89e
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-04-03 11:06:15 +0200

    Calculadora básica

--- repositorio/Program.cs
+++ repositorio/Program.cs
@@ -3,7 +3,61 @@
 namespace repositorio {
     class Program {
         public static void Main(string[] args) {
-            Console.WriteLine("Hello world!");
+
+            Console.WriteLine("Calculadora básica");
+            Console.WriteLine("------------------");
+
+            string entrada, operacion;
+            double op1=0, op2 = 0;
+            string[] numeros;
+
+            do
+            {
+                Console.WriteLine("Introduzca operación: ");
+                entrada = Console.ReadLine();
+
+                if (entrada.Contains("+")) {
+                    operacion = "+";
+                    numeros = entrada.Split('+');
+                    op1 = Convert.ToDouble(numeros[0]);
+                    op2 = Convert.ToDouble(numeros[1]);
+                } else if (entrada.Contains("-")) {
+                    operacion = "-";
+                    numeros = entrada.Split("-");
+                    op1 = Convert.ToDouble(numeros[0]);
+                    op2 = Convert.ToDouble(numeros[1]);
+                } else if (entrada.Contains("*")) {
+                    operacion = "*";
+                    numeros = entrada.Split('*');
+                    op1 = Convert.ToDouble(numeros[0]);
+                    op2 = Convert.ToDouble(numeros[1]);
+                } else {
+                    operacion = entrada;
+                }
+
+                switch (operacion) {
+                    case "+":
+                        Console.WriteLine($"Resultado: {op1 + op2}");
+                        break;
+                    case "-":
+                        Console.WriteLine($"Resultado: {op1 - op2}");
+                        break;
+                    case "*":
+                        Console.WriteLine($"Resultado: {op1 * op2}");
+                        break;
+                    case "q":
+                        Console.WriteLine("Saliendo de la calculadora...");
+                        break;
+                    default:
+                        Console.WriteLine("Operación incorrecta");
+                        break;
+                }
+
+            } while (!operacion.Equals("q"));         
+
+
         }
+
     }
+
 }
