308482dc1fff24270f6bca9d5e3f6af17773268e
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-04-04 10:11:51 +0200

    Exception classes

---
+++ repositorio/CalculationException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repositorio
+{
+    public class CalculationException : Exception
+    {
+        private const string DefaultMessage = "Error durante el cálculo: inténtelo de nuevo";
+        public CalculationException() : base(DefaultMessage) { }
+
+        public CalculationException(string message) : base(message) { }
+
+        public CalculationException(Exception innerException) : base(DefaultMessage, innerException) { }
+
+        public CalculationException(string message, Exception innerException) : base(message, innerException) { } 
+        
+
+
+    }
+}
---
+++ repositorio/CalculationOperationNotSupportedException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repositorio
+{
+    public class CalculationOperationNotSupportedException : Exception
+    {
+        public string Operation { get; }
+
+        private const string DefaultMessage = "Error durante el cálculo: inténtelo de nuevo";
+        public CalculationOperationNotSupportedException() : base(DefaultMessage) { }
+
+        public CalculationOperationNotSupportedException(string message) : base(message) { }
+
+        public CalculationOperationNotSupportedException(Exception innerException) : base(DefaultMessage, innerException) { }
+
+        public CalculationOperationNotSupportedException(string message, Exception innerException) : base(message, innerException) { }
+
+        public CalculationOperationNotSupportedException(string operation, string message) : base(message) => Operation = operation;
+    }
+
+    
+}
